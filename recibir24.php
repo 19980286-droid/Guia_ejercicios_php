9732e5da77a994bfdd662234bf1850f9553b87ee
19980286-droid <19980286@clases.edu.sv>
2026-05-20 08:54:21 -0600

    ejercicio_24
    Elabore un formulario que solicite la edad actual de una persona y envíe la
    información a otro archivo PHP donde se muestre la edad que tendrá dentro de
    10 años.

---
+++ recibir24.php
@@ -0,0 +1,15 @@
+<?php
+
+$edad = $_POST['edad'];
+
+echo "<center>";
+echo "<body bgcolor='pink'>";
+
+$año = ($edad + 10);
+
+echo "dentro de 10 años usted tendra: " . $año . " años";
+
+echo "</center>";
+echo "</body>";
+
+?>
